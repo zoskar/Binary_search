3ff79616e1cd4156e897d0706654b209edfc16ed
zoskar <zoskar00@gmail.com>
2021-04-14 15:02:20 +0200

    14.04

---
+++ Check Power of Two.py
@@ -0,0 +1,11 @@
+class Solution:
+    def solve(self, n):
+        res = 0
+        while (True):
+            if 2 ** res > n:
+                return False
+            if 2 ** res == n:
+                return True
+            res += 1
+
+
