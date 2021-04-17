9df9fae59f13d9efe481a909c15eaf5189facd08
zoskar <zoskar00@gmail.com>
2021-04-17 23:50:49 +0200

    17.04

---
+++ Collatz Sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def solve(self, n):
+        result = 1
+        while n != 1:
+            if n % 2 == 0:
+                n = n / 2
+            else:
+                n = 3 * n + 1
+            result += 1
+
+        return result
+
+
+
+
+
+
